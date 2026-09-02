10ebe763f24fb09dfd07f436f9110517997da7b7
lsy110630 <lsy110630@gmail.com>
2026-09-02 21:02:06 +0900

    9월2일
    설명 화면 완성

--- 겜마고작품 이수연/Assets/Scenes/Title/explanation/ExplanationDirector.cs
+++ 겜마고작품 이수연/Assets/Scenes/Title/explanation/ExplanationDirector.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExplanationDirector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameObject scene1;
+    GameObject scene2;
+    GameObject scene3;
+    GameObject scene4;
+
     void Start()
     {
-        
+        this.scene1 = GameObject.Find("scene1");
+
+        this.scene2 = GameObject.Find("scene2");
+        this.scene2.SetActive(false);
+
+        this.scene3 = GameObject.Find("scene3");
+        this.scene3.SetActive(false);
+
+        this.scene4 = GameObject.Find("scene4");
+        this.scene4.SetActive(false);
     }
 
-    void Update()
+    public void next1()
     {
-        
+        this.scene2.SetActive(true);
+        this.scene1.SetActive(false);
+    }
+
+    public void next2()
+    {
+        this.scene3.SetActive(true);
+        this.scene2.SetActive(false);
+    }
+
+    public void next3()
+    {
+        this.scene4.SetActive(true);
+        this.scene3.SetActive(false);
+    }
+
+    public void finish()
+    {
+        SceneManager.LoadScene("MainGame");
     }
 }
